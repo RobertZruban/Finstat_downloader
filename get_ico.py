7fc0bc5583f31b3c01cdd992448d029c64d2a8ce
Róbert Zruban <zrubanrobert@gmail.com>
2023-11-20 23:17:58 +0100

    add column date

--- get_ico.py
+++ get_ico.py
@@ -1,8 +1,11 @@
 import requests
+#import connect_database
+from datetime import date
 from bs4 import BeautifulSoup
 
 last_page = 25000
 ico_list = []
+date_today = []
 
 for x in range(1,4):
     
@@ -12,14 +15,16 @@ for x in range(1,4):
     soup = BeautifulSoup(browser.text,'html.parser')
     tables = soup.find_all('table')
     for tr in tables:
-        for x in range(150):
+        for x in range(30):
             try:
                 #print(tables)
-                print(soup.find_all('a', {'class', 'truncate openwindow'})[x].get_text())
+                #print(soup.find_all('a', {'class', 'truncate openwindow'})[x].get_text())
                 ico_substring = str(soup.findAll('a', {'class' : 'truncate openwindow'})[x])
                 ico_final = ico_substring[ico_substring.find('/')+1:ico_substring.find('/')+11]
                 ico_list.append(ico_final)
+                date_today.append(date.today())
             except:
                 pass
-        
+            
+
 
