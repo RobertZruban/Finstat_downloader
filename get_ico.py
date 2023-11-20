92f13d3f5b3b2bcc3676bc270ed15cd7b3591205
Róbert Zruban <zrubanrobert@gmail.com>
2023-11-20 17:41:01 +0100

    create a draft of ico numbers crawler

---
+++ get_ico.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+last_page = 25000
+ico_list = []
+
+for x in range(1,4):
+    
+    url = 'https://finstat.sk/databaza-financnych-udajov?page=' + str(x)
+    print(url)
+    browser = requests.get(url)  
+    soup = BeautifulSoup(browser.text,'html.parser')
+    tables = soup.find_all('table')
+    for tr in tables:
+        for x in range(150):
+            try:
+                #print(tables)
+                print(soup.find_all('a', {'class', 'truncate openwindow'})[x].get_text())
+                ico_substring = str(soup.findAll('a', {'class' : 'truncate openwindow'})[x])
+                ico_final = ico_substring[ico_substring.find('/')+1:ico_substring.find('/')+11]
+                ico_list.append(ico_final)
+            except:
+                pass
+        
+
