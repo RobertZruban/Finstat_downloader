ed2bd77bce59d44e7cc1fd2b00de64fe377c2f69
Róbert Zruban <zrubanrobert@gmail.com>
2023-11-21 16:36:10 +0100

    delete unnecesarry code

--- finstat_login.py
+++ finstat_login.py
@@ -2,40 +2,51 @@ import chromedriver_autoinstaller
 from selenium import webdriver    
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.common.by import By
+
 chromedriver_autoinstaller.install() 
 
 file_path = 'login_info.txt'
 url = 'https://finstat.sk/Account/LogOn'
 
-try:
-    with open(file_path, 'r') as file:
-        # Read lines from the file
-        lines = file.readlines()
-        username = lines[0].strip()
-        password = lines[1].strip()
+def credentials(file_path = file_path):
+    try:
+        with open(file_path, 'r') as file:
+            # Read lines from the file
+            lines = file.readlines()
+            username = lines[0].strip()
+            password = lines[1].strip()
 
+    except FileNotFoundError:
+        print(f"The file {file_path} was not found.")
+        return None, None
 
-except FileNotFoundError:
-    print(f"The file {file_path} was not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        return None, None
+    
+    return username, password
 
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
-
-
-# Create Chromeoptions instance 
-options = webdriver.ChromeOptions() 
-options.add_argument("--disable-blink-features=AutomationControlled") 
-options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
-options.add_experimental_option("useAutomationExtension", False) 
-browser = webdriver.Chrome(options=options) 
-browser.get(url)
+# Create a single instance of the browser
+def get_browser():
+    options = webdriver.ChromeOptions() 
+    options.add_argument("--disable-blink-features=AutomationControlled") 
+    options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+    options.add_experimental_option("useAutomationExtension", False) 
+    browser_instance = webdriver.Chrome(options=options) 
+    return browser_instance
 
 # login
-try:
-    input_field_name = browser.find_element(By.ID, 'Email')
-    input_field_name.send_keys(f"{username}")
-    input_field_password = browser.find_element(By.ID, 'Password')
-    input_field_password.send_keys(f"{password}")
-    input_field_password.send_keys(Keys.RETURN)
-except Exception as e:
-    print(f"Error has occured {e}")
+def login():
+    try:
+        username, password = credentials()
+        browser = get_browser()
+        browser.get(url)
+        input_field_name = browser.find_element(By.ID, 'Email')
+        input_field_name.send_keys(username)
+        input_field_password = browser.find_element(By.ID, 'Password')
+        input_field_password.send_keys(password)
+        input_field_password.send_keys(Keys.RETURN)
+        return browser
+    except Exception as e:
+        print(f"Error has occurred: {e}")
+
