a8f8838ef7f0c6aedf77670eda1e61a91daf01e5
Róbert Zruban <zrubanrobert@gmail.com>
2023-11-21 16:33:10 +0100

    create for loop logic to extract htmls

--- balance_sheet.py
+++ balance_sheet.py
@@ -1,3 +1,17 @@
-from finstat_login import login
-run_login = login()
+import finstat_login
+import database
+from bs4 import BeautifulSoup 
 
+companies_table = database.get_ico_table()
+run_login = finstat_login.login()
+
+
+
+# for loop to get balance sheet 
+for company in range(database.get_ico_len()):
+    current_company = companies_table['ico'][company]
+    url = f"https://finstat.sk/{current_company}/suvaha"
+    run_login.get(url)
+    html_source = run_login.page_source  
+    soup = BeautifulSoup(html_source,'html.parser')
+    print(soup)
